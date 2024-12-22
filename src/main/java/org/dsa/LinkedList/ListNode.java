19f2769c79c4379d50e5296cfa9d3a11d0da71e2
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-12-22 16:38:11 +0530

    intersection of LL

---
+++ src/main/java/org/dsa/LinkedList/ListNode.java
@@ -0,0 +1,12 @@
+package org.dsa.LinkedList;
+
+public class ListNode {
+        int data;
+        public ListNode next;
+
+        ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
