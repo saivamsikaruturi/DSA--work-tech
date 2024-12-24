0d6aef82ec54b96495794ea4dbf97d1d68e32423
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-12-25 00:17:33 +0530

    palindrome ll

---
+++ src/main/java/org/dsa/LinkedList/LinkedListInPlaceReversal/PalindromeLL.java
@@ -0,0 +1,58 @@
+package org.dsa.LinkedList.LinkedListInPlaceReversal;
+
+import org.dsa.LinkedList.ListNode;
+import org.dsa.LinkedList.PrintLinkedList;
+
+public class PalindromeLL {
+    public static boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast!=null){
+            slow = slow.next;
+        }
+
+        slow = reverseList(slow);
+
+        fast = head;
+
+        while(slow!=null){
+            if(fast.data==slow.data){
+                fast=fast.next;
+                slow = slow.next;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prevNode = null;
+        ListNode currentNode = head;
+        while(currentNode!=null){
+            ListNode nextNode=currentNode.next;
+            currentNode.next= prevNode;
+            prevNode = currentNode;
+            currentNode= nextNode;
+
+        }
+        head = prevNode;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+        boolean result = isPalindrome(head);
+        System.out.println(result);
+    }
+}
