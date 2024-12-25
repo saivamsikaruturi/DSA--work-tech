78ab7a9c92d56053409b39bcae1d4247b6d223b4
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-12-25 19:40:40 +0530

    balanced parenthesis

---
+++ src/main/java/org/dsa/Stacks/BalancedParenthesis.java
@@ -0,0 +1,30 @@
+package org.dsa.Stacks;
+
+import java.util.Stack;
+
+public class BalancedParenthesis {
+    static boolean isBalancedParentheses(String str) {
+        Stack<Character> stack =new Stack<>();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='{' || str.charAt(i)=='[' || str.charAt(i)=='('){
+                stack.push(str.charAt(i));
+            }
+            else if( (str.charAt(i)=='}' && !stack.isEmpty() && stack.peek()=='{') || (str.charAt(i)==']'&& !stack.isEmpty() && stack.peek()=='[') || (str.charAt(i)==')' && !stack.isEmpty() && stack.peek()=='(')){
+                stack.pop();
+            }
+            else{
+                stack.push(str.charAt(i));
+            }
+        }
+        if(stack.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBalancedParentheses("()[]{}"));
+    }
+}
