b510c7d819a2a1351f1ccf0c70ed9478d0895a76
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-12-26 23:50:33 +0530

    print ll in reverse order

---
+++ src/main/java/org/dsa/LinkedList/PrintLLinReverseOrder.java
@@ -0,0 +1,35 @@
+package org.dsa.LinkedList;
+
+public class PrintLLinReverseOrder {
+    void printInReverse(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        // At this point, 'prev' is the new head of the reversed linked list
+        while (prev != null) {
+            System.out.print(prev.data + " ");
+            prev = prev.next;
+        }
+
+    }
+    public static void main(String[] args) {
+        // Create a sample linked list
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // Print the linked list in reverse order
+        PrintLLinReverseOrder solution = new PrintLLinReverseOrder();
+        System.out.print("Linked list in reverse order: ");
+        solution.printInReverse(head);
+    }
+}
