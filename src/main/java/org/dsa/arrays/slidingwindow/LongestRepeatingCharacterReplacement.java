48b6691dfb6fcc128992a14c5f542157200fff60
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-12-28 13:26:33 +0530

    longest repeating character replacement

--- src/main/java/org/dsa/arrays/slidingwindow/LongestRepeatingCharacterReplacement.java
+++ src/main/java/org/dsa/arrays/slidingwindow/LongestRepeatingCharacterReplacement.java
@@ -1,4 +1,33 @@
 package org.dsa.arrays.slidingwindow;
 
 public class LongestRepeatingCharacterReplacement {
+    public static void main(String[] args) {
+        System.out.println(longestRepeatingCharacterString("AABABCC",2));
+    }
+
+    private static int longestRepeatingCharacterString(String s,int k) {
+        int[] freq = new int[26];
+        int left = 0;
+        int right = 0;
+        int maxWindow = 0;
+        int maxFreq = 0;
+
+        while(right < s.length()){
+
+            freq[s.charAt(right)-'A']++;
+
+            maxFreq = Math.max(maxFreq,freq[s.charAt(right)-'A']);
+
+            int windowLength = right - left +1;
+
+            if(windowLength - maxFreq > k){
+                freq[s.charAt(left)-'A']--;
+                left++;
+            }
+            windowLength = right-left+1;
+            maxWindow = Math.max(maxWindow,windowLength);
+            right++;
+        }
+        return maxWindow;
+    }
 }
