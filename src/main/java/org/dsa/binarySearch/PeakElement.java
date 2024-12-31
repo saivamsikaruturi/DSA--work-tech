401729835cd5a2ba49c78fa4140e9d5dad9a9fc8
VAMSI <krishnavamsikaruturi8@gmail.com>
2024-12-31 12:46:40 +0530

    peak element

---
+++ src/main/java/org/dsa/binarySearch/PeakElement.java
@@ -0,0 +1,24 @@
+package org.dsa.binarySearch;
+
+public class PeakElement {
+        public int findPeakElement(int[] nums) {
+            int left = 0;
+            int right = nums.length-1;
+            while(left<right){
+                int mid = left+(right-left)/2;
+                if(nums[mid]<nums[mid+1])
+                {
+                    left = mid+1;
+                }
+                else{
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+    public static void main(String[] args) {
+
+    }
+    }
+
