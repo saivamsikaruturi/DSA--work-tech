49743e99b42a9b3b685cb0bf459423f3dac9c2d8
VAMSI <krishnavamsikaruturi8@gmail.com>
2025-01-05 12:14:46 +0530

    remove duplicates from sorted list

--- src/main/java/org/dsa/linkedlist/RemoveDuplicatesFromSortedList.java
+++ src/main/java/org/dsa/linkedlist/RemoveDuplicatesFromSortedList.java
@@ -11,7 +11,7 @@ public class RemoveDuplicatesFromSortedList {
 
         while(current != null){
 
-            // if the dataue is duplicated
+            // if the element is duplicated
             if(current.next != null && current.data == current.next.data){
 
                 //iterate until the elements are same.
